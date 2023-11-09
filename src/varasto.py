947b0cf8b221ae67155702794349fd2176a21611
Vesa Rautiainen <veskurau@gmail.com>
2023-11-09 15:38:03 +0200

    Add pylint to Github actions

--- src/varasto.py
+++ src/varasto.py
@@ -49,3 +49,5 @@ class Varasto:
 
     def __str__(self):
         return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
+
+# TOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOSI PITKÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ RIVIIIIIIIIIIIIIIIiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii
